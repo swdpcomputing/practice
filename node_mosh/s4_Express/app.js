faa11c2aa755819caae45b9132bf94c68f1e99b4
swdpcomputing <swdpcomputing@outlook.com>
2020-11-06 16:28:16 +0000

    More practice with nodeMosh

--- node_mosh/s4_Express/app.js
+++ node_mosh/s4_Express/app.js
@@ -1,5 +1,14 @@
+const Joi = require('joi');
 const express = require('express');
+
 const app = express();
+app.use(express.json()) // adding middleware
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+];
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -10,21 +19,42 @@ app.get('/api/courses', (req, res) => {
 })
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id)
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found')
+    res.send(course);
+})
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    
+    const result = Joi.validate(req.body, schema);
+    console.log(result)
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 })
 
 app.get('/api/posts/:year/:month', (req, res) => {
     // multiple parameters
-    res.send(req.params.id)
+    res.send({"year": req.params.year, "month": req.params.month})
+})
 
-    // get query string
-    res.send(req.query)
+app.get('/api/posts/:year?sortyear', (req, res) => {
+    // multiple parameters
+    res.send({"year": req.params.year, "sortyear": req.query.sortyear})
 })
 
 //////
 
 const port = process.env.PORT || 3000;
+const hostname = '127.0.0.1';
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}..`);
+app.listen(port, hostname, () => {
+    console.log(`Listening on port ${port}...`);
 });
