7fa7f248fa8d10be8eb700150bf23e3e7d6b4530
swdpcomputing <67058346+swdpcomputing@users.noreply.github.com>
2020-11-08 21:19:03 +0000

    Finished Express basic. Started vidly.

--- node_mosh/s4_Express/app.js
+++ node_mosh/s4_Express/app.js
@@ -10,45 +10,70 @@ const courses = [
     {id: 3, name: 'course3'},
 ];
 
+// get base
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+// get courses
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 })
 
+// get course
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with the given ID was not found')
+    if (!course) return res.status(404).send('The course with the given ID was not found')
     res.send(course);
 })
 
+// post course
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    
-    const result = Joi.validate(req.body, schema);
-    console.log(result)
+    const { error } = validateCourse(req.body); // gets result error
+    if (error) return res.status(400).send(error.details[0].message);
 
     const course = {
         id: courses.length + 1,
         name: req.body.name
     };
+    
     courses.push(course);
     res.send(course);
 })
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    // multiple parameters
-    res.send({"year": req.params.year, "month": req.params.month})
-})
+//  put course
+app.put("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found')
 
-app.get('/api/posts/:year?sortyear', (req, res) => {
-    // multiple parameters
-    res.send({"year": req.params.year, "sortyear": req.query.sortyear})
-})
+    const { error } = validateCourse(req.body); // gets result error
+    if (error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course)
+});
+
+// delete course
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found')
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1) // delete
+
+    res.send(course);
+});
+
+
+//////
+
+const validateCourse = (course) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    
+    return schema.validate(course);
+}
 
 //////
 
