06fbc07b9a29079977e476aa215acfe9bc6dd789
swdpcomputing <67058346+swdpcomputing@users.noreply.github.com>
2020-11-27 19:51:13 +0000

    Completed 8: Querying Documents

--- node_mosh/mongo-demo/index.js
+++ node_mosh/mongo-demo/index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect("mongodb://localhost/playground")
+    .connect("mongodb://localhost:27017/playground", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log("Connected to MongoDB..."))
     .catch((err) => console.error("Could not connect to MongoDB", err));
 
@@ -27,4 +30,14 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+    // const courses = await Course.find(); // All
+    const courses = await Course
+        .find({ author: "Mosh" }) // Filtered
+        .limit(10) // Limit return num
+        .sort({ name: 1 }) // Sort
+        .select({ name: 1, tags: 1 }); // What gets returned
+    console.log(courses);
+}
+
+getCourses();
