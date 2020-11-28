3c809f477ffb8870881ded0bc2851b8659ac59aa
swdpcomputing <67058346+swdpcomputing@users.noreply.github.com>
2020-11-28 20:41:20 +0000

    Completed Exercise 1

---
+++ node_mosh/mongo-demo/exercises1.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+mongoose
+    .connect("mongodb://localhost:27017/mongo-exercises", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((err) => console.error("Could not connect to MongoDB", err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: Date,
+    isPublished: Boolean,
+    price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function getFilteredCourses() {
+    return await Course.find({ isPublished: true, tags: "backend" })
+        .sort({ name: 1 }) // Sort
+        .select({ name: 1, author: 1 }); // What gets returned
+}
+
+async function run() {
+    const courses = await getFilteredCourses();
+    console.log(courses);
+}
+
+run();
