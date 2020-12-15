b3f2a914a232d64426aef5c07e5b8164e738039c
swdpcomputing <67058346+swdpcomputing@users.noreply.github.com>
2020-12-15 20:18:18 +0000

    Completed 4: Embedding Documents

--- node_mosh/vidly/embedding.js
+++ node_mosh/vidly/embedding.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect("mongodb://localhost/playground")
+    .connect("mongodb://localhost/playground", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log("Connected to MongoDB..."))
     .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const authorSchema = new mongoose.Schema({
+    // Can apply validation to any of these.]
     name: String,
     bio: String,
     website: String,
@@ -17,6 +21,11 @@ const Course = mongoose.model(
     "Course",
     new mongoose.Schema({
         name: String,
+        // author: authorSchema, // without validation
+        author: {
+            type: authorScheme,
+            required: true,
+        },
     })
 );
 
@@ -35,4 +44,36 @@ async function listCourses() {
     console.log(courses);
 }
 
-createCourse("Node Course", new Author({ name: "Mosh" }));
+async function updateAuthor(courseId) {
+    const course = await Course.findById(courseId);
+    course.author.name = "Mosh Hamedani";
+    course.save();
+}
+
+async function updateAuthorV2(courseId) {
+    const course = await Course.updateOne(
+        { _id: courseId },
+        {
+            $set: {
+                "author.name": "Bob Jones",
+            },
+        }
+    );
+}
+
+async function removeSubdocument(courseId) {
+    const course = await Course.updateOne(
+        { _id: courseId },
+        {
+            $unset: {
+                // "author.name": "", // remove the nested property
+                author: "", // remove subdocument as a whole
+            },
+        }
+    );
+}
+
+// createCourse("Node Course", new Author({ name: "Mosh" }));
+// updateAuthor("5fd9132fe58135386c579920");
+// updateAuthorV2("5fd9132fe58135386c579920");
+removeSubdocument("5fd9132fe58135386c579920");
