a39d36e7b371a6f55dcc69dff1cc5ebe7e18508e
Stephen Powell <swdpcomputing@outlook.com>
2021-02-14 08:35:14 +0000

    12 - Testing Objects

--- node_mosh/11-unit-testing/tests/lib.test.js
+++ node_mosh/11-unit-testing/tests/lib.test.js
@@ -49,3 +49,12 @@ describe("getCurrencies", () => {
         expect(result).toEqual(expect.arrayContaining(["EUR", "USD", "AUD"]));
     });
 });
+
+describe("getProduct", () => {
+    it("should return the product with the given id", () => {
+        const result = lib.getProduct(1);
+        expect(result).toEqual({ id: 1, price: 10 }); // Too specific
+        expect(result).toMatchObject({ id: 1, price: 10 }); // Passes even with extra kvs
+        expect(result).toHaveProperty("id", 1);
+    });
+});
