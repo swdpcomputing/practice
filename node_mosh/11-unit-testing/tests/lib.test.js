829e9c4f0bc81f23f833cb603472de2a3596767f
Stephen Powell <swdpcomputing@outlook.com>
2021-02-14 16:12:07 +0000

    13 - Testing exceptions

--- node_mosh/11-unit-testing/tests/lib.test.js
+++ node_mosh/11-unit-testing/tests/lib.test.js
@@ -58,3 +58,20 @@ describe("getProduct", () => {
         expect(result).toHaveProperty("id", 1);
     });
 });
+
+describe("registerUser", () => {
+    it("should throw if username is falsy", () => {
+        const args = [null, undefined, NaN, "", 0, false];
+        args.forEach((a) => {
+            expect(() => {
+                lib.registerUser(a);
+            }).toThrow();
+        });
+    });
+
+    it("should return a user object if valud username is passed", () => {
+        const result = lib.registerUser("steve");
+        expect(result).toMatchObject({ username: "steve" });
+        expect(result.id).toBeGreaterThan(0);
+    });
+});
